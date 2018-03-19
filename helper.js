497ebd56d294f95c42463b68670f19b2e3f8f81f
Anastasiya_Kavalionak <Anastasiya_Kavalionak@epam.com>
2018-03-19 13:47:15 +0300

    upgrade methods

--- helper.js
+++ helper.js
@@ -20,18 +20,30 @@ class Helper {
 
     setTextInputValue(text, element) {
         logger.info(`set text input ${element} value ${text}`);
-        return helper.elementHelper.waitForVisibilityOf(element)
-            .then(() => helper.elementHelper.focusOn(element))
-            .then(() => helper.elementHelper.clear(element))
-            .then(() => helper.elementHelper.typeTextIn(text, element));
+        return this.elementHelper.waitForVisibilityOf(element)
+            .then(() => this.elementHelper.focusOn(element))
+            .then(() => this.elementHelper.clear(element))
+            .then(() => this.elementHelper.typeTextIn(text, element));
     }
 
     scrollAndWaitAndClick(element, scrollFlag) {
         logger.info(`scroll and wait and click ${element}`);
-        return helper.elementHelper.waitForVisibilityOf(element)
-            .then(() => scrollFlag ? helper.browserHelper.scrollTo(helper.elementHelper.getCoordinatesOf(element)) : {})
-            .then(() => helper.elementHelper.waitForClickabilityOf(element))
-            .then(() => helper.elementHelper.clickOn(element));
+        return this.elementHelper.waitForVisibilityOf(element)
+            .then(() => scrollFlag ? this.browserHelper.scrollTo(this.elementHelper.getCoordinatesOf(element)) : {})
+            .then(() => this.elementHelper.waitForClickabilityOf(element))
+            .then(() => this.elementHelper.clickOn(element));
+    }
+
+    scrollAndWaitAndClickAndCheckTitle(element, scrollFlag, title) {
+        logger.info(`scroll and wait and click ${element} and checked title should be ${title}`);
+        return this.scrollAndWaitAndClick(element, scrollFlag)
+            .then(() => this.browserHelper.checkTitle(title));
+    }
+
+    scrollAndWaitAndClickAndWaitTitleIs(element, scrollFlag, title) {
+        logger.info(`scroll and wait and click ${element} and checked title should be ${title}`);
+        return this.scrollAndWaitAndClick(element, scrollFlag)
+            .then(() => this.browserHelper.waitTitleIs(title));
     }
 }
 
